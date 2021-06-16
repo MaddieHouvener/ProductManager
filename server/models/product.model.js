4e984a38851ba203e98883587ae331c143a09b97
MaddieHouvener <73913585+MaddieHouvener@users.noreply.github.com>
2021-06-16 11:11:11 -0700

    algos

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -5,15 +5,15 @@ const ProductSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, "Title is required"],
-        minlength: [3, "Title must be 3 or more characters"] 
+        minlength: [3, "Title must be 3 or more characters"]
     },
 
-    price: { 
+    price: {
         type: String,
         required: [true, "Price is required"],
         minlength: [4, "Price must be at least 4 characters long"]
     },
-    
+
     description: {
         type: String,
         minlength: [5, "description must be more than 5 characters"]
